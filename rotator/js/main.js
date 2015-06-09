d02af7851de6fc0b45c81ff0063ae45e8d18ef4c
Tom Lacey-Johnson <tom.lacey-johnson@plymouth.ac.uk>
2015-06-09 14:06:19 +0100

    In development

---
+++ rotator/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$("#demo").vc3dEye({
+	imagePath:"img/slices/", // the location where you’ve put the images.
+	totalImages:1858, // the number of images you have.
+	imageExtension:"jpg" // the extension of the images. Make sure all the images have same extension.
+	});
+	
+});
